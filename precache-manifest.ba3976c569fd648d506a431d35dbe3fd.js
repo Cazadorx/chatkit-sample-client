e734530c3cb233910e502ae8a960040e17cc9098
Zan Markan <zan.markan@gmail.com>
2018-11-27 15:38:02 +0000

    Updates

---
+++ precache-manifest.ba3976c569fd648d506a431d35dbe3fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e69e97797c188b636bf",
+    "url": "/chatkit-sample-client/static/js/runtime~main.5e69e977.js"
+  },
+  {
+    "revision": "72aac3726aa6a0fa962a",
+    "url": "/chatkit-sample-client/static/js/main.72aac372.chunk.js"
+  },
+  {
+    "revision": "27f5c79231334e969f23",
+    "url": "/chatkit-sample-client/static/js/1.27f5c792.chunk.js"
+  },
+  {
+    "revision": "72aac3726aa6a0fa962a",
+    "url": "/chatkit-sample-client/static/css/main.97fec000.chunk.css"
+  },
+  {
+    "revision": "3329a5b0314e0398791a170467df806d",
+    "url": "/chatkit-sample-client/index.html"
+  }
+];
