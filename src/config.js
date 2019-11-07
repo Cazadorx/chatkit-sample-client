17e67c2b212785ddd72f3fed7b4a81f73492b5f9
Cazadorx <40477468+Cazadorx@users.noreply.github.com>
2019-11-07 14:41:17 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-const auth0Domain = 'pusher-chatkit-demo.auth0.com'
-const auth0ClientID = 'VF0kHM9szMj0aQGEfqafrdAKVwRb2a3Q'
-const auth0RedirectUri = 'https://chatkit-sample.netlify.com/'
+const auth0Domain = 'dev-bq7tr8d4.auth0.com'
+const auth0ClientID = '36vyLxUemMmJ9hEi0I2W9nNzOKKspKsE'
+const auth0RedirectUri = 'https://ubv-trujillo-eje-cacique-pitijoc.netlify.com'
 // const auth0RedirectUri = 'http://localhost:3000' /* Testing locally */
 
 const chatkitTokenProviderEndpoint = '.netlify/functions/token'
 
-const chatkitInstanceLocator = 'v1:us1:7954c374-f491-4c08-b71e-5abfc0a3dc89'
+const chatkitInstanceLocator = 'v1:us1:6b1c7e92-a90e-4504-840d-a66fa9e669d2'
 
 export { auth0Domain, auth0ClientID, auth0RedirectUri, chatkitTokenProviderEndpoint, chatkitInstanceLocator }
