521b9652f6e6c4a66d383c29ab35ca372dad4467
Cazadorx <40477468+Cazadorx@users.noreply.github.com>
2019-11-30 14:59:53 -0400

    Update index.js

--- src/components/UserHeader/index.js
+++ src/components/UserHeader/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from './index.module.css'
+import { Link } from 'react-router-dom';
 
 const placeholder =
   'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
@@ -11,7 +12,7 @@ export const UserHeader = ({ user = {} }) => (
       <h3>{user.name}</h3>
       <h5>{user.id && `${user.id}`}</h5>
 
-<input type="button" onclick="location.href='https://YOUR_DOMAIN/v2/logout?federated';" value="log out" />
+<Link to = 'https://google.com/'><button>log out</button></Link>
     
 </div>
 
