936299662bf86045678273ed13108be94e8273a9
Cazadorx <40477468+Cazadorx@users.noreply.github.com>
2019-11-30 16:30:27 -0400

    Update index.js

--- src/components/UserHeader/index.js
+++ src/components/UserHeader/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import style from './index.module.css'
 import { Link } from 'react-router-dom';
-import {logout} from 'src/auth/auth.js'
 
 const placeholder =
   'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
@@ -13,8 +12,16 @@ export const UserHeader = ({ user = {} }) => (
       <h3>{user.name}</h3>
       <h5>{user.id && `${user.id}`}</h5>
 
+  logout() {
+    // Clear Access Token and ID Token from local storage
+    localStorage.removeItem('access_token')
+    localStorage.removeItem('id_token')
+    localStorage.removeItem('expires_at')
+    localStorage.removeItem('chatkit_user')
+ 
+
 <button
-                onClick={() => auth.logout()}
+                onClick={() => logout()}
                
               >
                 Log Out
