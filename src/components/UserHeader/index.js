9a712c01deeb25b667ef130942e2af90304abd92
Cazadorx <40477468+Cazadorx@users.noreply.github.com>
2019-11-30 15:07:42 -0400

    Update index.js

--- src/components/UserHeader/index.js
+++ src/components/UserHeader/index.js
@@ -12,7 +12,34 @@ export const UserHeader = ({ user = {} }) => (
       <h3>{user.name}</h3>
       <h5>{user.id && `${user.id}`}</h5>
 
-<Link to = 'https://google.com/'><button>log out</button></Link>
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
     
 </div>
 
