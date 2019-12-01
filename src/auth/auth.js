5e030a3bac6728bece1aeda3020936f92e025a53
Cazadorx <40477468+Cazadorx@users.noreply.github.com>
2019-12-01 14:40:14 -0400

    Update auth.js

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -63,15 +63,7 @@ export default class Auth {
     localStorage.setItem('expires_at', expiresAt)
   }
 
-  /*logout() {
-    // Clear Access Token and ID Token from local storage
-    localStorage.removeItem('access_token')
-    localStorage.removeItem('id_token')
-    localStorage.removeItem('expires_at')
-    localStorage.removeItem('chatkit_user')
-    //TODO: callback to reset the app state.
-  } 
-  */
+
 const logout = () => {
 // Clear Access Token and ID Token from local storage
 localStorage.removeItem('access_token')
