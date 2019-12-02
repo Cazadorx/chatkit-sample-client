e28707429f565f59241d53cf2f4a7b056b099755
Cazadorx <40477468+Cazadorx@users.noreply.github.com>
2019-12-02 06:00:21 -0400

    Update auth.js

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -62,7 +62,7 @@ export default class Auth {
     localStorage.setItem('id_token', authResult.idToken)
     localStorage.setItem('expires_at', expiresAt)
   }
- logOut() => {
+ logOut(){
 // Clear Access Token and ID Token from local storage
 localStorage.removeItem('access_token')
 localStorage.removeItem('id_token')
